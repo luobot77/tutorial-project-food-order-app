971b98911aa996581ca25f4b7bdebb1e9f3576d7
Jason <jsyzmq9328@gmail.com>
2023-07-10 00:13:17 +0800

    feat: add cartReducer on <CartProvider /> component

--- src/store/CartProvider.tsx
+++ src/store/CartProvider.tsx
@@ -1,18 +1,64 @@
+import { Reducer, useReducer } from 'react';
 import CartContext, { TCartContext } from './cart-context';
 import type { TCartItem } from '@/types';
 
+type TCartState = {
+  items: TCartItem[];
+  totalAmount: number;
+};
+
+enum ECartActionType {
+  ADD = 'ADD',
+  REMOVE = 'REMOVE',
+}
+
+type TCartAction = { type: ECartActionType.ADD; item: TCartItem } | { type: ECartActionType.REMOVE; id: string };
+
+const initialCartState: TCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer: Reducer<TCartState, TCartAction> = (state, action) => {
+  switch (action.type) {
+    case ECartActionType.ADD: {
+      const updatedItems = state.items.concat(action.item); // concat 返回新的 array
+      const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+
+      return {
+        items: updatedItems,
+        totalAmount: updatedTotalAmount,
+      };
+    }
+    case ECartActionType.REMOVE: {
+      // TODO: 
+      return state;
+    }
+    default:
+      throw new Error('cartReducer: unknown cart action type');
+  }
+};
+
 const CartProvider = ({ children }: { children: React.ReactNode }) => {
+  const [cartState, dispatchCartAction] = useReducer(cartReducer, initialCartState);
+
   const handleAddItemToCart = (item: TCartItem) => {
-    // ...
+    dispatchCartAction({
+      type: ECartActionType.ADD,
+      item,
+    });
   };
 
   const handleRemoveItemFromCart = (id: string) => {
-    // ...
+    dispatchCartAction({
+      type: ECartActionType.REMOVE,
+      id,
+    });
   };
 
   const cartContext: TCartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: handleAddItemToCart,
     removeItem: handleRemoveItemFromCart,
   };
