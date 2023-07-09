92c3da54d32595035570b5e1f46faa61264a92a5
Jason <jsyzmq9328@gmail.com>
2023-07-09 23:50:12 +0800

    feat: use cartCtx by useContext

--- src/components/Layout/HeaderCartButton.tsx
+++ src/components/Layout/HeaderCartButton.tsx
@@ -1,14 +1,26 @@
+import { useContext } from 'react';
+import CartContext from '@/store/cart-context';
 import styles from './HeaderCartButton.module.css';
 import CartIcon from '@/components/Cart/CartIcon';
 
 const HeaderCartButton = ({ onClick }: { onClick: () => void }) => {
+  const cartCtx = useContext(CartContext);
+
+  if (!cartCtx) {
+    throw new Error('HeaderCartButton: CartContext is not defined');
+  }
+
+  const numberOfCartItems = cartCtx.items.reduce((cartNumber, item) => {
+    return cartNumber + item.amount;
+  }, 0);
+
   return (
     <button className={styles.button} onClick={onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={styles.badge}>3</span>
+      <span className={styles.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
